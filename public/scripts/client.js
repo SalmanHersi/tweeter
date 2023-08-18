13ce32e676e1e109727a6efe269e0f5847b57ec8
SalmanHersi <hersi.salman@gmail.com>
2023-08-17 20:11:14 -0400

    added document.ready to client.js file to wait for index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,43 +4,71 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const renderTweets = function (tweets) {
-  const $tweetsContainer = $("#tweets-container");
+$(document).ready(function () {
+  const renderTweets = function (tweets) {
+    const $tweetsContainer = $(".tweets-container");
+    console.log("check");
+    console.log(tweets);
+    // Empty the container to avoid duplicate rendering
+    $tweetsContainer.empty();
 
-  // Empty the container to avoid duplicate rendering
-  $tweetsContainer.empty();
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      console.log($tweet);
+      $tweetsContainer.append($tweet);
+      console.log($tweetsContainer);
+    }
+  };
 
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $tweetsContainer.append($tweet);
-  }
-};
+  const createTweetElement = function (tweet) {
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <div class="user">
+            <img src="${tweet.user.avatars}" alt="${tweet.user.name}" />
+            <span class="name">${tweet.user.name}</span>
+          </div>
+          <span class="handle">${tweet.user.handle}</span>
+        </header>
+        <div class="content">${tweet.content.text}</div>
+        <footer>
+          <span class="timestamp">${tweet.created_at}</span>
+          <div class="icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
 
-const createTweetElement = function (tweet) {
-  const $tweet = $(`
-    <article class="tweet">
-      <header>
-        <div class="user">
-          <img src="${tweet.user.avatars}" alt="${tweet.user.name}" />
-          <span class="name">${tweet.user.name}</span>
-        </div>
-        <span class="handle">${tweet.user.handle}</span>
-      </header>
-      <div class="content">${tweet.content.text}</div>
-      <footer>
-        <span class="timestamp">${timeago.format(tweet.created_at)}</span>
-        <div class="icons">
-          <i class="fas fa-flag"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="fas fa-heart"></i>
-        </div>
-      </footer>
-    </article>
-  `);
+    return $tweet;
+  };
 
-  return $tweet;
-};
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
 
-const data = [];
-
-renderTweets(data);
+  renderTweets(data);
+});
